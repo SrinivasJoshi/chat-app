ede80923ba95b450bcc1bd39298dd0ae98cf82b5
Srinivas Joshi <srinivasjoshi60@gmail.com>
2023-04-11 15:02:27 +0530

    next-auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+import Button from '@/components/ui/Button';
 import { db } from '@/lib/db';
 
 export default async function Home() {
-	await db.set('hello', 'hello');
-	return <div className='text-red-500'>HIiii</div>;
+	return <Button variant={'ghost'}>Hii</Button>;
 }
