071a1d9a18d11f35cd52ea109a59c80e92a2ffe1
Srinivas Joshi <srinivasjoshi60@gmail.com>
2023-04-11 14:16:33 +0530

    db,styles setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Button from '@/components/ui/Button';
+import { FC } from 'react';
+interface DashboardProps {}
+
+const Dashboard: FC<DashboardProps> = () => {
+	return <Button> Hello</Button>;
+};
+
+export default Dashboard;
